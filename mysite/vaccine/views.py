34853f6236ab453901607de319b969116fcebb29
Buruk Kassahun <brookkasahun@gmail.com>
2025-08-07 21:00:55 +0300

    implemented pagginaition for the list views for storage, centers and vaccines

--- mysite/vaccine/views.py
+++ mysite/vaccine/views.py
@@ -5,13 +5,19 @@ from django.http import Http404, HttpResponseRedirect
 from vaccine.forms import VaccineForm
 from django.urls import reverse
 from django.shortcuts import get_object_or_404
-
+from django.core.paginator import Paginator
 # Create your views here.
 class VaccineList(View):
     def get(self, request):
-        vaccine_list = Vaccine.objects.all()
+        vaccine_list = Vaccine.objects.all().order_by("name")
+        paginator = Paginator(vaccine_list, 2)
+        page_number = request.GET.get("page")
+        page_obj = paginator.get_page(page_number)
+        
+
+
         context = {
-            "objects_list": vaccine_list
+            "page_obj": page_obj
         }
         return render(request, "vaccine/vaccine-list.html", context)
 
